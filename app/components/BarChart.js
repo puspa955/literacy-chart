6ad61e79024bf3bafdfffccd53e01ab82e67471e
puspa955 <58810154+puspa955@users.noreply.github.com>
2024-10-20 11:01:57 +0545

    Update BarChart.js

--- app/components/BarChart.js
+++ app/components/BarChart.js
@@ -2,7 +2,7 @@
 import React from "react";
 import * as d3 from "d3";
 import Scale from "./ChartUtils";
-import ChartLabels from "./Chartcomponents"; 
+import ChartLabels from "./ChartComponents"; 
 
 const BarChart = ({ data, title, yLabel = "Population" }) => {
   const margin = { top: 60, right: 10, bottom: 60, left: 70 };
